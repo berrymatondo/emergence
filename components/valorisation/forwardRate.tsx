1e9036d7cfb74c91f3f4b03bf95a01e425e039a3
jmatondo <j_matondo@hotmail.com>
2024-08-27 18:10:11 +0200

    xxx

--- components/valorisation/forwardRate.tsx
+++ components/valorisation/forwardRate.tsx
@@ -16,18 +16,18 @@ const ForwardRate = ({ forwardrates }: ForwardRateProps) => {
       <TableHeader>
         <TableRow>
           <TableHead className="text-left mx-0 pl-0 pr-2">Tenor</TableHead>
-
-          <TableHead className="  mx-0 px-0">Rate</TableHead>
-          <TableHead className="text-right  mx-0 px-0">Label</TableHead>
+          {/*           <TableHead className="  mx-0 px-0">Rate</TableHead>
+           */}{" "}
+          <TableHead className="text-right  mx-0 px-0">Rate</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {forwardrates?.map((yc: any) => (
           <TableRow key={yc.id}>
             <TableCell className="font-medium  mx-0 px-0">{yc.tenor}</TableCell>
-
-            <TableCell className="  mx-0 px-0">{yc.rate} %</TableCell>
-            <TableCell className="text-right  mx-0 px-0">{yc.label}</TableCell>
+            {/*             <TableCell className="  mx-0 px-0">{yc.rate} %</TableCell>
+             */}{" "}
+            <TableCell className="text-right  mx-0 px-0">{yc.rate} %</TableCell>
           </TableRow>
         ))}
       </TableBody>
